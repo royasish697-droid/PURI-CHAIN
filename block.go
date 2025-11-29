0dacae2a4b5b7a49e763662c03480201e8eb5bd0
royasish697-droid <royasish697@gmail.com>
2025-11-29 12:58:48 +0530

    Update block.go

--- block.go
+++ block.go
@@ -8,29 +8,35 @@ import (
 	"time"
 )
 
+type Transaction struct {
+	From      string `json:"from"`
+	To        string `json:"to"`
+	Amount    int    `json:"amount"`
+	Signature string `json:"signature"`
+}
+
 type Block struct {
-	Index        int            `json:"index"`
-	Timestamp    string         `json:"timestamp"`
-	Transactions []Transaction  `json:"transactions"`
-	PrevHash     string         `json:"prevHash"`
-	Hash         string         `json:"hash"`
-	Nonce        int            `json:"nonce"`
-	Difficulty   int            `json:"difficulty"`
+	Index        int           `json:"index"`
+	Timestamp    string        `json:"timestamp"`
+	Transactions []Transaction `json:"transactions"`
+	PrevHash     string        `json:"prevHash"`
+	Hash         string        `json:"hash"`
+	Nonce        int           `json:"nonce"`
+	Difficulty   int           `json:"difficulty"`
+}
+
+func serializeTxs(txs []Transaction) string {
+	res := ""
+	for _, t := range txs {
+		res += t.From + t.To + fmt.Sprintf("%d", t.Amount) + t.Signature
+	}
+	return res
 }
 
 func calculateHash(b Block) string {
 	record := fmt.Sprintf("%d%s%s%s%d", b.Index, b.Timestamp, serializeTxs(b.Transactions), b.PrevHash, b.Nonce)
 	h := sha256.Sum256([]byte(record))
 	return hex.EncodeToString(h[:])
-}
-
-func serializeTxs(txs []Transaction) string {
-	// simple serialization for hashing
-	res := ""
-	for _, t := range txs {
-		res += t.From + t.To + fmt.Sprintf("%d", t.Amount) + t.Signature
-	}
-	return res
 }
 
 func mineBlock(old Block, txs []Transaction, difficulty int) Block {
